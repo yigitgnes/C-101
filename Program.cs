c72f6919703413d95b0294d1fe2b18be3e935c02
Yigit Mustafa Gunes <84538553+yigitgnes@users.noreply.github.com>
2021-07-25 22:47:37 +0300

    C# Started

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adınızı Giriniz: ");
+            string name = Console.ReadLine();
+            Console.Write("Soy Adınızı Giriniz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hoş Geldiniz "+ name +" "+ surname+"!");
+        }
+    }
+}
